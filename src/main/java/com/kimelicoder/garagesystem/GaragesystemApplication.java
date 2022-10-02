b43b5aae953c1465ca64e76e6c485c1fc6631c6e
Kimeli <jobkimeli2016@gmail.com>
2022-10-02 16:37:40 +0300

    Initial commit

---
+++ src/main/java/com/kimelicoder/garagesystem/GaragesystemApplication.java
@@ -0,0 +1,13 @@
+package com.kimelicoder.garagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimelicoder/garagesystem/GaragesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimelicoder.garagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
